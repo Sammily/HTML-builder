eafc9d605a5ae4a800cfe49e2c08347e0e9c00df
Sammily <sammily03@yandex.ru>
2022-05-23 14:17:55 +0300

    fix: change order of commands

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,14 +6,13 @@ const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 
 stdout.write('Enter text:\n');
 stdin.on('data', data => {
-  output.write(data);
   if(data.toString().trim() === 'exit') {
     console.log('You typed EXIT and we say goodbye!');
     process.exit();
   }
   else{
+    output.write(data);
     console.log('Added!');
-    process.on('exit', () => process.exit());
     process.on('SIGINT', function() {
       console.log('Bye!');
       process.exit();
