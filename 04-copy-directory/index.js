0054aea5edf91d28fb49666453cfbb05b1c1368a
Sammily <sammily03@yandex.ru>
2022-05-25 22:17:22 +0300

    fix: add async to 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -6,24 +6,39 @@ const dirCopy = path.join(__dirname, 'files-copy');
 
 fsPromises.mkdir(dirCopy, { recursive: true });
 
-fs.readdir(dirCopy, (error, files) => {
-  if (error) throw error;
-  files.forEach(file => {
-    fs.unlink(path.join(dirCopy, file), error => {
-      if(error) throw error;
-    }); 
+async function deleteFiles(){
+  fs.readdir(dirCopy, (error, files) => {
+    if (error) throw error;
+    files.forEach(file => {
+      fs.unlink(path.join(dirCopy, file), error => {
+        if(error) throw error;
+      }); 
+    });
   });
-});
+}
 
-fs.readdir(dir, (error, files) => {
-  if (error) throw error;
-  files.forEach(file => {
-    fsPromises.copyFile(path.join(dir, file), path.join(dirCopy, file))
-      .then(function() {
-        console.log(`File ${file} copied`);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+function copyFiles(){
+  fs.readdir(dir, (error, files) => {
+    if (error) throw error;
+    files.forEach(file => {
+      fsPromises.copyFile(path.join(dir, file), path.join(dirCopy, file))
+        .then(function() {
+          console.log(`File ${file} copied`);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    });
   });
-});
+}
+
+async function copyDirectory() {
+  try {
+    await deleteFiles();
+    copyFiles();
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+copyDirectory();
